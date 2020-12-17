5d8cc05cabab0f05784ea6dc690e3b485656bceb
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-12-18 01:11:13 +0700

    First Init UAS PRAK SKD

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,123 @@
+<?php 
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\UserModel;
+use App\Models\AuthModel;
+
+class Auth extends Controller
+{
+    public function register()
+    {
+
+        $val = $this->validate(
+            [
+            'username' => 'required',
+            'password' => 'required'
+            ]
+            );
+            var_dump($val);
+            if (!$val) {
+            $pesanvalidasi = \Config\Services::validation();
+            return redirect()->to('/register')->withInput()->with('validate',
+            $pesanvalidasi);
+            }
+
+        $db = \Config\Database::connect();
+        
+
+
+        //PROSES ENKRIPSI PASSWORD KE DALAM DATABASE
+        $pass=$this->request->getPost('password');
+        $key = 5;
+        for ($i = 0; $i < strlen($pass); $i++) {
+        $kode[$i] = ord($pass[$i]); //rubah ASCII ke desimal
+        $b[$i] = ($kode[$i] + $key) % 256; //proses enkripsi                
+        $c[$i] = chr($b[$i]); //rubah desimal ke ASCII
+        }
+
+        $encrypt = '';
+            for ($i = 0; $i < strlen($pass); $i++) {
+            echo $c[$i];
+            $encrypt = $encrypt . $c[$i];
+        }
+
+    
+        $data = array(
+            'username' => $this->request->getPost('username'),
+            'password' => $encrypt,
+            'role' => $this->request->getPost('role')
+        );
+        $model  = new UserModel();
+        $model->insert($data);
+        session()->setFlashdata('pesan', 'selamat anda berhasil registrasi');
+        return redirect()->to('/');
+    }
+    public function login()
+    {
+        $model = new AuthModel();
+        $table = 'users';
+        $username = $this->request->getPost('username');
+        $password = $this->request->getPost('password');
+
+    
+        $row = $model->get_data_login($username, $table); 
+
+        //PROSES DEKRIPSI SAAT LOGIN
+        $pass = $row->password;
+        $key = 5;
+        for($i=0;$i<strlen($pass);$i++)
+        {
+        $kode[$i]=ord($pass[$i]); // rubah ASII ke desimal
+        $b[$i]=($kode[$i] - $key ) % 256; // proses dekripsi Caesar
+        $c[$i]=chr($b[$i]); //rubah desimal ke ASCII
+        }
+
+        $decyrpt = '';
+        for ($i=0;$i<strlen($pass);$i++){
+        echo $c[$i];
+        $decyrpt = $decyrpt.$c[$i];
+        }
+
+        if ($row == NULL || $password!=$decyrpt) {
+            session()->setFlashdata('pesan', 'Username atau Password anda salah');
+            return redirect()->to('/');
+        }else{
+            //PROSES DEKRIPSI DI SAAT LOGIN
+            $pass = $row->password;
+            $key = 5;
+            for($i=0;$i<strlen($pass);$i++)
+            {
+            $kode[$i]=ord($pass[$i]); // rubah ASII ke desimal
+            $b[$i]=($kode[$i] - $key ) % 256; // proses dekripsi Caesar
+            $c[$i]=chr($b[$i]); //rubah desimal ke ASCII
+            }
+
+            $decyrpt = '';
+            for ($i=0;$i<strlen($pass);$i++){
+            echo $c[$i];
+            $decyrpt = $decyrpt.$c[$i];
+            }
+
+            if ($decyrpt==$password) {
+                $data = array(
+                'log' => TRUE,
+                'username' => $row->username,
+                'role' => $row->role,
+                );
+                session()->set($data);
+                session()->setFlashdata('pesan', 'Anda Berhasil login');
+                return redirect()->to('/backend');
+            }
+        }
+    }
+    public function logout()
+    {
+        $session = session();
+        $session->destroy();
+        session()->setFlashdata('pesan', 'Berhasil logout');
+        return redirect()->to('/');
+    }
+}
+?>
---
+++ app/Controllers/Backend.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\Controller;
+class Backend extends BaseController
+{
+    public function index()
+    {
+    echo view('backend/landing_page');
+    }
+}
